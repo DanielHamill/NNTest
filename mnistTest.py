7677dceac5d93fac4c8e39f350f8c830899cfa99
DanielHamill <DanielHamill00@gmail.com>
2018-03-17 02:17:15 +0100

    working user drawn digits

--- mnistTest.py
+++ mnistTest.py
@@ -1,5 +1,13 @@
 import tensorflow as tf
+import matplotlib.pyplot as plt
+import matplotlib.image as img
+import numpy as np
 from tensorflow.examples.tutorials.mnist import input_data
+from sys import platform
+
+if platform == "win32":
+    import win_unicode_console as wc
+    wc.enable()
 
 mnist = input_data.read_data_sets('tmp/data/', one_hot=True)
 
@@ -40,31 +48,68 @@ def net_model():
     output = tf.matmul(layer3, weightOut) + biasOut
     return output
 
-def train_net():
-    prediction = net_model()
-    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=prediction, labels=y_))
+def train_net(sess):
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=prediction, labels=y_))
     optimizer = tf.train.AdamOptimizer().minimize(cost)
+    print("start training")
+    # with tf.Session() as sess:
 
-    with tf.Session() as sess:
-        sess.run(tf.global_variables_initializer())
+    sess.run(tf.global_variables_initializer())
 
-        print("start")
+    for epoch in range(n_epochs):
+        total_cost = 0
+        for current_batch in range(int(mnist.train.num_examples/batch_size)):
+            x, y = mnist.train.next_batch(batch_size)
+            _, c = sess.run([optimizer, cost], feed_dict={x_: x, y_: y})
 
-        for epoch in range(n_epochs):
-            total_cost = 0
-            for current_batch in range(int(mnist.train.num_examples/batch_size)):
-                x, y = mnist.train.next_batch(batch_size)
-                _, c = sess.run([optimizer, cost], feed_dict={x_: x, y_: y})
+            total_cost += c
 
-                total_cost += c
+        print('Epoch', epoch, 'completed out of', n_epochs,'loss:', total_cost)
 
-            print('Epoch', epoch, 'completed out of', n_epochs,'loss:', total_cost)
+    correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y_, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
+    print('Accuracy:',accuracy.eval({x_:mnist.test.images, y_:mnist.test.labels}, session=sess))
 
+def feed_net_mnist(sess):
+    x = int(input('input index:'))
 
-        correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y_, 1))
-        accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
-        print('Accuracy:',accuracy.eval({x_:mnist.test.images, y_:mnist.test.labels}))
+    digit = mnist.test.images[x]
+    img = np.array(digit)
+    pixels = img.reshape((28, 28))
+    plt.imshow(pixels, cmap='gray')
+    plt.show()
 
+    # with tf.Session() as sess:
+    result = tf.argmax(prediction, 1)
+    print(sess.run(result, feed_dict={x_: [digit], y_: [mnist.test.labels[x]]}))
 
+def feed_net_file(sess, file_path):
+    raw = img.imread(file_path)
+    rgba = np.array(raw)
+    greyscale = []
+    for i in range(len(rgba)):
+        for j in range(len(rgba[0])):
+            greyscale.append(rgba[i][j][0])
+    greyscale = np.array(greyscale)
 
-train_net()
+    # print(greyscale)
+
+    pixels = greyscale.reshape((28, 28))
+    plt.imshow(pixels, cmap='gray')
+    plt.show()
+
+    # with tf.Session() as sess:
+    result = tf.argmax(prediction, 1)
+    print(sess.run(result, feed_dict={x_: [greyscale], y_: [mnist.test.labels[0]]}))
+    return
+
+with tf.Session() as sess:
+    n_epochs = 1
+    5
+    prediction = net_model()
+    train_net(sess)
+
+    while input('press enter')!='end':
+        # feed_net_mnist(sess)
+        fil = 'digit' + input('input digit') + '.png'
+        feed_net_file(sess,fil)
